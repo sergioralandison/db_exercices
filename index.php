b8cc3f755449927df2f0e88812c8293589d93848
Sergio <sergioralandison@outlook.com>
2020-08-17 15:31:13 +0200

    :triangular_flag_on_post: code update

--- index.php
+++ index.php
@@ -47,27 +47,6 @@ echo "</table>";
 
 /*--------------------------------Requête n°6 : mettre à jour l'adresse email de la nouvelle personne--------------------------------*/
 
-echo "<table style='border: solid 1px black;'>";
-echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
-
-class TableRowsB extends RecursiveIteratorIterator {
-  function __construct($it) {
-    parent::__construct($it, self::LEAVES_ONLY);
-  }
-
-  function current() {
-    return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
-  }
-
-  function beginChildren() {
-    echo "<tr>";
-  }
-
-  function endChildren() {
-    echo "</tr>" . "\n";
-  }
-}
-
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -75,19 +54,23 @@ $dbname = "db_exercices";
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $stmt = $conn->prepare("UPDATE `datas` SET `email` = 'swilliams@gmail.com' WHERE `first_name` = 'Sara' AND `last_name` = 'Williams'");
+
+  $sql = "UPDATE `datas` SET `email` = 'swilliams@gmail.com' WHERE `first_name` = 'Sara' AND `last_name` = 'Williams'";
+
+  // Prepare statement
+  $stmt = $conn->prepare($sql);
+
+  // execute the query
   $stmt->execute();
 
-  // set the resulting array to associative
-  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-  foreach(new TableRowsB(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-    echo $v;
-  }
+  // echo a message to say the UPDATE succeeded
+  echo $stmt->rowCount() . " records UPDATED successfully";
 } catch(PDOException $e) {
-  echo "Error: " . $e->getMessage();
+  echo $sql . "<br>" . $e->getMessage();
 }
+
 $conn = null;
-echo "</table>";
 
 ?>
