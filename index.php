1bbff90fabcfbd406d6b1fe4a21d1f824b4fca73
Sergio <sergioralandison@outlook.com>
2020-08-17 15:20:44 +0200

    :triangular_flag_on_post: update code

--- index.php
+++ index.php
@@ -2,42 +2,26 @@
 
 /*--------------------------------Requête n°7 : supprimer la nouvelle personne--------------------------------*/
 
-echo "<table style='border: solid 1px black;'>";
-echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
-
-class TableRows2 extends RecursiveIteratorIterator {
-  function __construct($it) {
-    parent::__construct($it, self::LEAVES_ONLY);
-  }
-
-  function current() {
-    return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
-  }
-
-  function beginChildren() {
-    echo "<tr>";
-  }
-
-  function endChildren() {
-    echo "</tr>" . "\n";
-  }
-}
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_exercices";
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $stmt = $conn->prepare("DELETE FROM `datas` WHERE `first_name` = 'Sara' AND `last_name` = 'Williams'");
-  $stmt->execute();
 
-  // set the resulting array to associative
-  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-  foreach(new TableRows2(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-    echo $v;
-  }
+  // sql to delete a record
+  $sql = "DELETE FROM `datas` WHERE `first_name` = 'Sara' AND `last_name` = 'Williams'";
+
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  echo "Record deleted successfully";
 } catch(PDOException $e) {
-  echo "Error: " . $e->getMessage();
+  echo $sql . "<br>" . $e->getMessage();
 }
+
 $conn = null;
-echo "</table>";
 
 ?>
