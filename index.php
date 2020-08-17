5b071235f4e942e0c01edc7e61561bb9186a8589
Sergio <sergioralandison@outlook.com>
2020-08-17 15:25:51 +0200

    :triangular_flag_on_post: update code

--- index.php
+++ index.php
@@ -2,27 +2,6 @@
 
 /*--------------------------------Requête n°5 : Insérer une nouvelle personne dans la liste--------------------------------*/
 
-echo "<table style='border: solid 1px black;'>";
-echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
-
-class TableRows extends RecursiveIteratorIterator {
-  function __construct($it) {
-    parent::__construct($it, self::LEAVES_ONLY);
-  }
-
-  function current() {
-    return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
-  }
-
-  function beginChildren() {
-    echo "<tr>";
-  }
-
-  function endChildren() {
-    echo "</tr>" . "\n";
-  }
-}
-
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -30,21 +9,17 @@ $dbname = "db_exercices";
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $stmt = $conn->prepare("INSERT INTO `datas`(`first_name`, `last_name`, `email`, `gender`)
-	                        VALUES ('Sara', 'Williams', 'swilliams@icloud.com', 'Female')");
-  $stmt->execute();
-
-  // set the resulting array to associative
-  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-  foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-    echo $v;
-  }
+  $sql = "INSERT INTO `datas`(`first_name`, `last_name`, `email`, `gender`)
+          VALUES ('Sara', 'Williams', 'swilliams@icloud.com', 'Female')";
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  echo "New record created successfully";
 } catch(PDOException $e) {
-  echo "Error: " . $e->getMessage();
+  echo $sql . "<br>" . $e->getMessage();
 }
+
 $conn = null;
-echo "</table>";
-echo "<br>";
 
 ?>
