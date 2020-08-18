a5bf3f5535bce32a4d2298d895c20c222ca35064
Sergio <sergioralandison@outlook.com>
2020-08-18 09:32:25 +0200

    :triangular_flag_on_post: code update

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 /*--------------------------------Requête n°5 : Insérer une nouvelle personne dans la liste--------------------------------*/
 
-  $sql = "INSERT INTO `datas`(`first_name`, `last_name`, `email`, `gender`)
-          VALUES ('Sara', 'Williams', 'swilliams@icloud.com', 'Female')";
+  $sql = "INSERT INTO `datas_2`(`name`, `lastname`, `birth_date`)
+          VALUES ('John', 'Doe', 2000-08-18)";
   // use exec() because no results are returned
   $conn->exec($sql);
   echo "New record created successfully";
